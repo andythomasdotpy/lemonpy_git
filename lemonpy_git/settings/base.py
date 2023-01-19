d6c75583c6afad7374b661481e2e868873f2b02e
Andy <andythomas.py@gmail.com>
2023-01-18 19:20:01 -0600

    Start template running

--- lemonpy_git/settings/base.py
+++ lemonpy_git/settings/base.py
@@ -8,21 +8,11 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config("SECRET_KEY")
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
-
-
 # Application definition
 
 INSTALLED_APPS = [
+    "blog_app",
+
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
