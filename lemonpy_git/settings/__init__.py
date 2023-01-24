38cc5738c3df75d72ac6c03459cbf8a18a639972
Andy <andythomas.py@gmail.com>
2023-01-23 21:02:40 -0600

    Updated settings/__init__.py

--- lemonpy_git/settings/__init__.py
+++ lemonpy_git/settings/__init__.py
@@ -1,7 +1,17 @@
+import os
 from decouple import config
 
 
-if config("SETTINGS-DEV-OR-PROD") == "prod":
-    from .prod import *
-else:
+# SETTINGS_PROD = "prod"
+
+# os.environ.get('TWITTER_ACCESS_TOKEN')
+try:
+    print(f"printing **before** {config('SETTINGS_PATH')}")
+
+    if config('SETTINGS_PATH') == "prod":
+        print("prod.py settings activated!")
+        from .prod import *
+except:
+    print("SETTINGS_PATH not equal to prod..")
+    print("dev.py settings activated!")
     from .dev import *
