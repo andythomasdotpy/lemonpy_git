7f0d5fa31517d897af24f8eebe7d88e10e0770bd
Andy <andythomas.py@gmail.com>
2023-02-03 09:28:39 -0600

    Update admin path

--- lemonpy_git/urls.py
+++ lemonpy_git/urls.py
@@ -8,7 +8,6 @@ from django.conf import settings
 
 urlpatterns = [
     path("", include("blog_app.urls")),
-    # path("admin/", admin.site.urls),
     path(config("ADMIN"), admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) \
   + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
